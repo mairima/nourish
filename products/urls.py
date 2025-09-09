67d7c8c0be56b29951f45a5c1145d4e70d50e49f
mairima <aissatou.mairi@gmail.com>
2025-09-09 03:53:25 +0200

    Add app_name for URL namespacing

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+app_name = 'products' 
 urlpatterns = [
     path('', views.all_products, name='products_index'),
     path('<product_id>', views.product_detail, name='product_detail'),
