8299eae587bc520ae2bad82c0764fba8f04a84a9
mairima <aissatou.mairi@gmail.com>
2025-09-12 18:54:52 +0200

    Replace pillow image with cloudinary and remove sku

--- products/models.py
+++ products/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
 class Category(models.Model):
@@ -17,13 +18,11 @@ class Category(models.Model):
 
 class Product(models.Model):
     category = models.ForeignKey(Category, null=True, blank=True, on_delete=models.SET_NULL)
-    sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
-    image_url = models.URLField(max_length=1024, null=True, blank=True)
-    image = models.ImageField(upload_to='products/', null=True, blank=True)
+    image = CloudinaryField('image', default='placeholder', null=True, blank=True)
 
     def __str__(self):
         return self.name
