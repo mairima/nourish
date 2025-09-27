c10cd84ae7523ce1dade35b05d23e6d11c060ad5
mairima <aissatou.mairi@gmail.com>
2025-09-28 01:25:32 +0200

    Update settings and URLs for deployment

--- nourish/urls.py
+++ nourish/urls.py
@@ -1,19 +1,8 @@
-"""Nourish URL Configuration
+﻿"""Nourish URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-"""Nourish URL Configuration"""
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
@@ -27,6 +16,7 @@ urlpatterns = [
     path("products/", include(("products.urls", "products"), namespace="products")),
     path("bag/", include("bag.urls")),
     path("checkout/", include("checkout.urls")),
+    path("profile/", include("profiles.urls")),
     path("dashboard/", TemplateView.as_view(template_name="home/index.html"), name="dashboard"),
 ]
 
