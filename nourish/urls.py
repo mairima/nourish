25c0539dbc78cfdd85de68b85b0c97e430849cc3
mairima <aissatou.mairi@gmail.com>
2025-09-28 18:06:37 +0200

    Update nourish urls apps

--- nourish/urls.py
+++ nourish/urls.py
@@ -22,4 +22,10 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("allauth.urls")),
     path('newsletter/', include('newsletter.urls')),
+    path("", include("home.urls")),
+    path("products/", include("products.urls")),
+    path("bag/", include("bag.urls")),
+    path("checkout/", include("checkout.urls")),
+    path("profiles/", include("profiles.urls")),
+    
 ]
