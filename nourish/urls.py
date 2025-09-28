c6694d654456f8a56fe2e5aedff73bfc798a4f65
mairima <aissatou.mairi@gmail.com>
2025-09-28 17:43:00 +0200

    Refactor urls.py

--- nourish/urls.py
+++ nourish/urls.py
@@ -1,24 +1,25 @@
-﻿"""Nourish URL Configuration
+"""Nourish URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+"""Nourish URL Configuration"""
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
 from django.conf.urls.static import static
-from django.views.generic import TemplateView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("allauth.urls")),
-    path("", include("home.urls")),  # <-- this registers name='home'
-    path("products/", include(("products.urls", "products"), namespace="products")),
-    path("bag/", include("bag.urls")),
-    path("checkout/", include("checkout.urls")),
-    path("profile/", include("profiles.urls")),
-    path("dashboard/", TemplateView.as_view(template_name="home/index.html"), name="dashboard"),
+    path('newsletter/', include('newsletter.urls')),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
