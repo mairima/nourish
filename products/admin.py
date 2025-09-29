832b27e1b98a7553c5302100547dacb41c7e12d4
mairima <aissatou.mairi@gmail.com>
2025-09-29 22:48:38 +0200

    Chore(Admin): Show product images via new image_filename/image_url

--- products/admin.py
+++ products/admin.py
@@ -1,25 +1,22 @@
+# products/admin.py
 from django.contrib import admin
+from django.utils.html import format_html
 from .models import Product, Category
 
-# Register your models here.
-
 class ProductAdmin(admin.ModelAdmin):
-    list_display = (
-        'name',
-        'category',
-        'price',
-        'rating',
-        'image',
-    )
+    list_display = ('name', 'category', 'price', 'rating', 'image_preview')
+    list_filter = ('category',)
+    search_fields = ('name', 'description')
 
-    ordering = ('name',)
+    def image_preview(self, obj):
+        url = obj.static_image_url if obj.image_filename else (obj.image_url or '')
+        if not url:
+            return '-'
+        return format_html('<img src="{}" style="height:40px; width:auto;" />', url)
+    image_preview.short_description = 'Image'
 
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = (
-        'friendly_name',
-        'name',
-    )
+    list_display = ('friendly_name', 'name')
 
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Category, CategoryAdmin)
-# Register your models here.
