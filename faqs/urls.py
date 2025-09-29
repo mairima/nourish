592a1fedbd7ee4ee8c6d27d7952fdf28ecfc1be9
mairima <aissatou.mairi@gmail.com>
2025-09-29 19:23:09 +0200

    Update faqs/urls.py with namespaced routes.

--- faqs/urls.py
+++ faqs/urls.py
@@ -1,10 +1,11 @@
-# faqs/urls.py
 from django.urls import path
 from . import views
 
+app_name = "faqs"
+
 urlpatterns = [
-    path("", views.faqs, name="faqs"),
-    path("add_faq", views.add_faq, name="add_faq"),
-    path("update_faq/<int:id>/", views.update_faq, name="update_faq"),
-    path("delete_faq/<int:id>/", views.delete_faq, name="delete_faq"),
+    path("", views.faqs, name="index"),
+    path("add/", views.add_faq, name="add_faq"),
+    path("update/<int:id>/", views.update_faq, name="update_faq"),
+    path("delete/<int:id>/", views.delete_faq, name="delete_faq"),
 ]
