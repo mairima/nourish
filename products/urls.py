fc9fa69af85b85d6228bf9b9abad17c15ef31067
mairima <aissatou.mairi@gmail.com>
2025-10-22 09:52:21 +0200

    products: add product_detail route and ensure namespacing

--- products/urls.py
+++ products/urls.py
@@ -7,4 +7,6 @@ app_name = "products"
 urlpatterns = [
     path("", views.all_products, name="products_index"),
     path("add/", views.add_product, name="add_product"),
+    path('edit/<int:product_id>/', views.edit_product, name='edit_product'),
+    path('<int:product_id>/', views.product_detail, name='product_detail'), 
 ]
