624dbea3efdcc24499192eb3263c44ca573b6267
mairima <aissatou.mairi@gmail.com>
2025-10-22 05:22:52 +0200

    Update profile URLs and ensure correct routing to profile and order history

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 
 urlpatterns = [
     path("", views.profile, name="profile"),
-    path("edit/", views.profile_edit, name="profile_edit"),
+    path("edit/", views.profile, name="profile_edit"),
+    path('order_history/<order_number>', views.order_history, name='order_history'),
 ]
