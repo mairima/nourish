af59355b955d90df76fe760100db9dc65a1c0348
mairima <aissatou.mairi@gmail.com>
2025-10-25 00:15:36 +0200

    Fix: Use Django default ClearableFileInput in ProductForm

--- products/forms.py
+++ products/forms.py
@@ -1,8 +1,14 @@
 from django import forms
+from django.forms.widgets import ClearableFileInput
 from .models import Product, Category
 
 
 class ProductForm(forms.ModelForm):
+    image = forms.ImageField(
+        label='Image',
+        required=False,
+        widget=ClearableFileInput()
+    )
 
     class Meta:
         model = Product
@@ -12,7 +18,7 @@ class ProductForm(forms.ModelForm):
         super().__init__(*args, **kwargs)
         categories = Category.objects.all()
         friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
-
         self.fields['category'].choices = friendly_names
-        for field_name, field in self.fields.items():
+
+        for _, field in self.fields.items():
             field.widget.attrs['class'] = 'border-black rounded-0'
