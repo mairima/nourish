23dbdd90ac8fe7c30075c8b683a456c53bde7c19
mairima <aissatou.mairi@gmail.com>
2025-10-29 11:49:59 +0100

    Update: Add view functions for Terms & Privacy pages

--- home/views.py
+++ home/views.py
@@ -5,3 +5,9 @@ def index(request):
     """ A view to return the index page """
     
     return render(request, 'home/index.html')
+
+def privacy_policy(request):
+    return render(request, 'terms/privacy_policy.html')
+
+def terms_conditions(request):
+    return render(request, 'terms/terms_conditions.html')
