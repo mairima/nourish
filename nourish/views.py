958a14ad2f25ea34ff4701fcb5fa89c475ce1332
mairima <aissatou.mairi@gmail.com>
2025-10-29 02:07:00 +0100

    Fix: Add custom 404 error handler view

--- nourish/views.py
+++ nourish/views.py
@@ -1,2 +1,8 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+
 def index(request): return HttpResponse("Products home")
+
+def handler404(request, exception):
+    """ Error Handler 404 - Page Not Found """
+    return render(request, "errors/404.html", status=404)
