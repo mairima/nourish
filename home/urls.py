fe90b1563e873e5b97535152adcf4b8bb6d121c9
mairima <aissatou.mairi@gmail.com>
2025-10-29 11:49:59 +0100

    Update: Add Terms & Privacy URLs

--- home/urls.py
+++ home/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
+    path('privacy-policy/', views.privacy_policy, name='privacy_policy'),
+    path('terms-conditions/', views.terms_conditions, name='terms_conditions'),
 ]
