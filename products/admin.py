01adf6c6546fcf66aa6a6baa3f690cbc38459d0d
mairima <aissatou.mairi@gmail.com>
2025-10-30 11:26:06 +0100

    Enhancement: Improve product admin with image previews and cleaner display

--- products/admin.py
+++ products/admin.py
@@ -3,20 +3,24 @@ from django.contrib import admin
 from django.utils.html import format_html
 from .models import Product, Category
 
+
+@admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('name', 'category', 'price', 'rating', 'image_preview')
     list_filter = ('category',)
     search_fields = ('name', 'description')
 
     def image_preview(self, obj):
-        url = obj.static_image_url if obj.image_filename else (obj.image_url or '')
-        if not url:
+        """
+        Show a small preview of the product image in admin.
+        Uses unified image_url property from the Product model.
+        """
+        if not obj.image_url:
             return '-'
-        return format_html('<img src="{}" style="height:40px; width:auto;" />', url)
+        return format_html('<img src="{}" style="height:40px; width:auto;" />', obj.image_url)
     image_preview.short_description = 'Image'
 
+
+@admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('friendly_name', 'name')
-
-admin.site.register(Product, ProductAdmin)
-admin.site.register(Category, CategoryAdmin)
