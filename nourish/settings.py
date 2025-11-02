23712ad7578a612f55da21b0bf0455220f3168f9
mairima <aissatou.mairi@gmail.com>
2025-11-02 08:09:10 +0100

    Fix: custom WhiteNoise storage to skip missing admin/fonts.css,

--- nourish/settings.py
+++ nourish/settings.py
@@ -210,7 +210,7 @@ STATIC_URL = "/static/"
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "custom_storage.IgnoreAdminManifestStorage"
 
 WHITENOISE_MANIFEST_STRICT = False
 WHITENOISE_SKIP_COMPRESS_EXTENSIONS = [
