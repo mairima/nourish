6b1164e126c4f5880e79b3b54818bd6af2d75a76
mairima <aissatou.mairi@gmail.com>
2025-11-12 01:38:55 +0100

    Fix newsletter admin for python linter

--- newsletter/admin.py
+++ newsletter/admin.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
+
 from .models import NewsletterSubscription
+
 
 @admin.register(NewsletterSubscription)
 class NewsletterSubscriptionAdmin(admin.ModelAdmin):
-    list_display = ("email", "is_active", "discount_code", "discount_expires")
+    """Admin configuration for the NewsletterSubscription model."""
+    list_display = (
+        "email",
+        "is_active",
+        "discount_code",
+        "discount_expires",
+    )
     search_fields = ("email", "discount_code")
