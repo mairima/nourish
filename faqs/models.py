7636694790b9ece778a40abadefdb059ae8e2bec
mairima <aissatou.mairi@gmail.com>
2025-11-12 01:32:17 +0100

    Fix faqs models for python linter

--- faqs/models.py
+++ faqs/models.py
@@ -2,9 +2,10 @@ from django.db import models
 
 
 class FAQ(models.Model):
-    """FAQs model: Frequently Asked Questions"""
+    """Model representing a frequently asked question."""
     question = models.CharField(max_length=100, null=False, blank=False)
     answer = models.TextField(null=False, blank=False)
 
     def __str__(self):
+        """Return the question text as string representation."""
         return self.question
