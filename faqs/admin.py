a88bb4d35526cf197e49ecc64b96b7fae3fe52e3
mairima <aissatou.mairi@gmail.com>
2025-11-12 01:31:00 +0100

    Fix faqs admin for python linter

--- faqs/admin.py
+++ faqs/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
+
 from .models import FAQ
 
 
 @admin.register(FAQ)
 class FAQAdmin(admin.ModelAdmin):
+    """Admin configuration for the FAQ model."""
     list_display = ("question",)
