f6afdcba340a1fbaa464d606b332243a52ea5ee4
mairima <aissatou.mairi@gmail.com>
2025-11-12 01:37:22 +0100

    Fix home urls for python linter

--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
+
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home'),
-    path('privacy-policy/', views.privacy_policy, name='privacy_policy'),
-    path('terms-conditions/', views.terms_conditions, name='terms_conditions'),
+    path("", views.index, name="home"),
+    path("privacy-policy/", views.privacy_policy, name="privacy_policy"),
+    path("terms-conditions/", views.terms_conditions, name="terms_conditions"),
 ]
