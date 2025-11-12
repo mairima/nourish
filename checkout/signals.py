f9b676af3d8929c271a0daef1ad26bda23694e25
mairima <aissatou.mairi@gmail.com>
2025-11-12 01:14:00 +0100

    Fix checkout signals for Python Linter

--- checkout/signals.py
+++ checkout/signals.py
@@ -3,17 +3,14 @@ from django.dispatch import receiver
 
 from .models import OrderLineItem
 
+
 @receiver(post_save, sender=OrderLineItem)
 def update_on_save(sender, instance, created, **kwargs):
-    """
-    Update order total on lineitem update/create
-    """
+    """Update order total when a line item is saved."""
     instance.order.update_total()
 
 
 @receiver(post_delete, sender=OrderLineItem)
 def update_on_delete(sender, instance, **kwargs):
-    """
-    Update order total on lineitem delete
-    """
+    """Update order total when a line item is deleted."""
     instance.order.update_total()
