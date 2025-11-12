c118f32ccd2f7ba264dd9782a253f83765700436
mairima <aissatou.mairi@gmail.com>
2025-11-12 01:25:46 +0100

    Fix contact models for python linter

--- contact/models.py
+++ contact/models.py
@@ -2,11 +2,12 @@ from django.db import models
 
 
 class Contact(models.Model):
-    """Contact model for users to contact admin"""
+    """Model for user contact messages sent to the admin."""
     name = models.CharField(max_length=75, null=False, blank=False)
     email = models.EmailField(null=False, blank=False)
     message = models.TextField(null=False, blank=False)
     sent = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
+        """Return the contact name as string representation."""
         return self.name
